85c1986c40c19581bfbf14b367b2deb486cfd3ec
marcelinosdo <marcelinotecnologia@outlook.com>
2020-07-25 17:13:49 -0300

    atualizaçao testes Palindrome, BodyMassIndex e LeapYear

--- src/test/java/school/cesar/eta/unit/PalindromeTest.java
+++ src/test/java/school/cesar/eta/unit/PalindromeTest.java
@@ -1,28 +1,31 @@
 package school.cesar.eta.unit;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PalindromeTest {
 
+    private Palindrome palindrome;
+
+    @BeforeEach
+    public void setupTest(){
+        palindrome = new Palindrome();
+    }
+
     @Test
-    public void check_palindromeWord_true() {
-        String word = "ANA";
-        boolean result = Palindrome.check(word);
+    public void palindrome_singleLetter_true(){
+        boolean result = palindrome.check("a");
         Assertions.assertTrue(result);
     }
-
     @Test
-    public void check_nonPalindromeWord_false() {
-        String word = "ABC";
-        boolean result = Palindrome.check(word);
+    public void palindrome_PalindromeWord_false(){
+        boolean result = palindrome.check("ab");
         Assertions.assertFalse(result);
     }
-
     @Test
-    public void check_emptyWord_true() {
-        String word = "";
-        boolean result = Palindrome.check(word);
+    public void palindrome_PalindromeWord_true(){
+        boolean result = palindrome.check("renner");
         Assertions.assertTrue(result);
     }
 
